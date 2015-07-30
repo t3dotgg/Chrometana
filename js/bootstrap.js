10d3fd47ffbd243558a7e2fa4af8dabe8da21014
Theo <Theo@TheoBrowne.com>
2015-07-29 21:18:17 -0700

    Listener for install logic

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     return { redirectUrl: details.url.replace("www.bing.com/search", "www.google.com/search")};
 }, {urls: ["*://www.bing.com/search*"]}, ["blocking"]);
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+});
