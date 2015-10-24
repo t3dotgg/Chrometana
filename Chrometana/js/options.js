91e6c056aae0fcd158ec3873ec31084a9526e9d4
Kyle Ladd <kladd6@gmail.com>
2015-10-24 12:08:14 -0400

    Custom Search Engine Toggle

--- Chrometana/js/options.js
+++ Chrometana/js/options.js
@@ -1,9 +1,17 @@
+var defaultSE = "Google.com";
 function save_options(key, element, value) {
   var options = {};
   options[key] = value;
-  if(key == "custom_engine"){
-    options.search_engine = "Custom";
+  if(key == "custom_engine"){ 
+    if(value != ""){
+      options.search_engine = "Custom";
+    }
+    else{
+      options['search_engine'] = defaultSE;
+    }
   }
+  console.log("options");
+  console.log(options);
   chrome.storage.sync.set(options, function() {
     restore_options();
   });
@@ -13,14 +21,24 @@ function save_options(key, element, value) {
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    search_engine: 'Google.com',custom_engine: ''
+    search_engine: defaultSE, 
+    custom_engine: ''
   }, function(items) {
+    // console.log("UPDATE DISPLAY");
+    // console.log(items);
     updateDisplay(items);
   });
 }
+
 function updateDisplay(items){
+  console.log("settings being saved");
+  console.log(items);
+  // console.log(items.search_engine);
   for (i = 0; i <  selectorList.length; i++) {
+    // console.log(selectorList[i].getAttribute('value'));
     if (selectorList[i].getAttribute('value') == items.search_engine) {
+      console.log("adding selected to");
+      console.log(selectorList[i]);
       addClass(selectorList[i], 'selected');
       // document.getElementById("webpage_diagram_placeholder").className = selectorList[i].getAttribute("data-icon-class");
     }
@@ -28,13 +46,21 @@ function updateDisplay(items){
       removeClass(selectorList[i], 'selected');
     }
   }
+  if(items.search_engine = "Custom"){
+    useCustomSearch.checked = true;
+  }
+  else{
+    useCustomSearch.checked = false;
+  }
   document.getElementById("custom_engine").value = items.custom_engine;
-
+  updateCustomSearchView();
   var status = document.getElementById('status');
+  addClass(status, 'updated');
     status.textContent = 'New search engine preferences saved.';
     setTimeout(function() {
       status.textContent = '';
-    }, 750);
+      removeClass(status, 'updated');
+    }, 1050);
 }
 
 //Parses arguments from URL
@@ -43,30 +69,33 @@ function getURLVariable(variable){
   var vars = query.split("&");
   for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split("=");
-    if(pair[0] == variable){return pair[1];}
+    if(pair[0] == variable){
+      return pair[1];
+    }
   }
   return(false);
 }
-
+var useCustomSearch = document.getElementById('useCustomSearch');
+var custom_engine_settings = document.getElementById('custom_engine_settings');
 var selectorList = document.getElementsByClassName('selector');
 
 document.addEventListener('DOMContentLoaded', restore_options);
 
 var optionCaller = function() {
-  save_options('search_engine',this, this.getAttribute('value'));
+  save_options('search_engine', this, this.getAttribute('value'));
 };
 
 var handleMouseover = function() {
-  selectedElements = document.getElementsByClassName('selected');
+  selectedElements = document.getElementsByClassName('hovering');
   for (i = 0; i <  selectedElements.length; i++) {
-    removeClass(selectedElements[i], 'selected');
+    removeClass(selectedElements[i], 'hovering');
   }
-  addClass(this, 'selected');
+  addClass(this, 'hovering');
 };
 
 var handleMouseout = function() {
-  removeClass(this, 'selected');
-  restore_options();
+  removeClass(this, 'hovering');
+  // restore_options();
 };
 
 if (getURLVariable("newinstall") == "yes"){
@@ -81,13 +110,31 @@ for (i = 0; i <  selectorList.length; i++) {
   selectorList[i].addEventListener('mouseleave', handleMouseout, false);
 }
 
-document.getElementById('custom_engine_update').addEventListener('click', function() {
-  var element = document.getElementById('custom_engine');
-  if(element.value.toLowerCase().contains("http")){
-    save_options('custom_engine',element,element.value);
+useCustomSearch.addEventListener('click', function() {
+  // console.log("triggered");
+  updateCustomSearchView();
+});
+
+function updateCustomSearchView(){
+  if(useCustomSearch.checked){
+    addClass(custom_engine_settings, 'visible');
   }
   else{
-    alert("Custom search engine must start with http");
+    removeClass(custom_engine_settings, 'visible');
+  }
+}
+document.getElementById('custom_settings_update').addEventListener('click', function() {
+  var element = document.getElementById('custom_engine');
+  if(useCustomSearch.checked){
+    if(element.value.toLowerCase().contains("http")){
+      save_options('custom_engine', element, element.value);
+    }
+    else{
+      alert("Custom search engine must start with http");
+    }
+  }
+  else{
+    save_options('custom_engine', element, "");
   }
 });
 
@@ -98,12 +145,11 @@ document.getElementById('additional-settings-toggle').addEventListener('click', 
 
   if (settingsPane.className.search('open') >= 0) {
     removeClass(settingsPane, 'open');
-    removeClass(engines, 'hidden')
-
-  } else {
+    removeClass(engines, 'hidden');
+  }
+  else {
     addClass(settingsPane, 'open');
-    addClass(engines, 'hidden')
-
+    addClass(engines, 'hidden');
   }
 });
 
@@ -116,6 +162,7 @@ function addClass(element, classNameToAdd) {
 function removeClass(element, classNameToAdd) {
   element.className = element.className.replace(classNameToAdd, '');
 }
+
 String.prototype.contains = function(text) {
   return this.indexOf(text) !== -1;
 };
