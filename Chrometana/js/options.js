7b499e2930f7ad46b9c19dcf9f7560396c25ab81
Kyle Ladd <kladd6@gmail.com>
2015-10-24 18:21:12 -0400

    Generic Checkbox settings - for now, just Open Website setting.

--- Chrometana/js/options.js
+++ Chrometana/js/options.js
@@ -1,85 +1,13 @@
 var defaultSE = "Google.com";
-function save_options(key, element, value) {
-  var options = {};
-  options[key] = value;
-  if(key == "custom_engine"){ 
-    if(value != ""){
-      options.search_engine = "Custom";
-    }
-    else{
-      options['search_engine'] = defaultSE;
-    }
-  }
-  console.log("options");
-  console.log(options);
-  chrome.storage.sync.set(options, function() {
-    restore_options();
-  });
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  chrome.storage.sync.get({
-    search_engine: defaultSE, 
-    custom_engine: ''
-  }, function(items) {
-    // console.log("UPDATE DISPLAY");
-    // console.log(items);
-    updateDisplay(items);
-  });
-}
-
-function updateDisplay(items){
-  console.log("settings being saved");
-  console.log(items);
-  // console.log(items.search_engine);
-  for (i = 0; i <  selectorList.length; i++) {
-    // console.log(selectorList[i].getAttribute('value'));
-    if (selectorList[i].getAttribute('value') == items.search_engine) {
-      console.log("adding selected to");
-      console.log(selectorList[i]);
-      addClass(selectorList[i], 'selected');
-      // document.getElementById("webpage_diagram_placeholder").className = selectorList[i].getAttribute("data-icon-class");
-    }
-    else {
-      removeClass(selectorList[i], 'selected');
-    }
-  }
-  if(items.search_engine = "Custom"){
-    useCustomSearch.checked = true;
-  }
-  else{
-    useCustomSearch.checked = false;
-  }
-  document.getElementById("custom_engine").value = items.custom_engine;
-  updateCustomSearchView();
-  var status = document.getElementById('status');
-  addClass(status, 'updated');
-    status.textContent = 'New search engine preferences saved.';
-    setTimeout(function() {
-      status.textContent = '';
-      removeClass(status, 'updated');
-    }, 1050);
-}
-
-//Parses arguments from URL
-function getURLVariable(variable){
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if(pair[0] == variable){
-      return pair[1];
-    }
-  }
-  return(false);
-}
+var advanced_settings = document.getElementsByClassName('chrometana_advanced_setting');
 var useCustomSearch = document.getElementById('useCustomSearch');
 var custom_engine_settings = document.getElementById('custom_engine_settings');
 var selectorList = document.getElementsByClassName('selector');
 
-document.addEventListener('DOMContentLoaded', restore_options);
+if (getURLVariable("newinstall") == "yes"){
+  var installadvice = document.getElementById('installadvice');
+  addClass(installadvice, 'visible');
+}
 
 var optionCaller = function() {
   save_options('search_engine', this, this.getAttribute('value'));
@@ -95,25 +23,93 @@ var handleMouseover = function() {
 
 var handleMouseout = function() {
   removeClass(this, 'hovering');
-  // restore_options();
 };
 
-if (getURLVariable("newinstall") == "yes"){
-  var installadvice = document.getElementById('installadvice');
-  addClass(installadvice, 'visible');
+var advancedSettingsCaller = function() {
+  var id = this.id;
+  var value;
+  if(this.getAttribute("type")=="checkbox"){
+    if(this.checked){
+      value = true;
+    }
+    else{
+      value = false;
+    }
+  }
+  save_options(this.id, this, value);
+};
+
+function save_options(key, element, value){
+  var options = {};
+  options[key] = value;
+  if(key == "custom_engine"){ 
+    if(value !== ""){
+      options.search_engine = "Custom";
+    }
+    else{
+      options.search_engine = defaultSE;
+    }
+  }
+  chrome.storage.sync.set(options, function() {
+    restore_options();
+  });
 }
 
-
-for (i = 0; i <  selectorList.length; i++) {
-  selectorList[i].addEventListener('click', optionCaller,false);
-  selectorList[i].addEventListener('mouseover', handleMouseover, false);
-  selectorList[i].addEventListener('mouseleave', handleMouseout, false);
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    search_engine: defaultSE, 
+    custom_engine: '',
+    enable_open_website: false
+  }, function(items) {
+    updateDisplay(items);
+  });
 }
 
-useCustomSearch.addEventListener('click', function() {
-  // console.log("triggered");
+function updateDisplay(items){
+  for (i = 0; i <  selectorList.length; i++) {
+    if (selectorList[i].getAttribute('value') == items.search_engine) {
+      addClass(selectorList[i], 'selected');
+    }
+    else {
+      removeClass(selectorList[i], 'selected');
+    }
+  }
+  if(items.search_engine == "Custom"){
+    useCustomSearch.checked = true;
+  }
+  else{
+    useCustomSearch.checked = false;
+  }
+  document.getElementById("custom_engine").value = items.custom_engine;
   updateCustomSearchView();
-});
+  updateCheckBoxes(items);
+  var status = document.getElementById('status');
+  addClass(status, 'updated');
+    status.textContent = 'New search engine preferences saved.';
+    setTimeout(function() {
+      status.textContent = '';
+      removeClass(status, 'updated');
+    }, 1050);
+}
+
+function updateCheckBoxes(items){
+  for (i = 0; i <  advanced_settings.length; i++) {
+    var id = advanced_settings[i].getAttribute("id");
+    if(id in items){
+      if(items[id]===true){
+        document.getElementById(id).checked = true;
+      }
+      else{
+        document.getElementById(id).checked = false;
+      }
+    }
+    else{
+      document.getElementById(id).checked = false;
+    }
+  }
+}
 
 function updateCustomSearchView(){
   if(useCustomSearch.checked){
@@ -123,7 +119,52 @@ function updateCustomSearchView(){
     removeClass(custom_engine_settings, 'visible');
   }
 }
-document.getElementById('custom_settings_update').addEventListener('click', function() {
+
+//Parses arguments from URL
+function getURLVariable(variable){
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){
+      return pair[1];
+    }
+  }
+  return(false);
+}
+
+function addClass(element, classNameToAdd) {
+  if (!element.className.includes(classNameToAdd)) {
+    element.className = element.className + ' ' + classNameToAdd;
+  }
+}
+
+function removeClass(element, classNameToAdd) {
+  element.className = element.className.replace(classNameToAdd, '');
+}
+
+String.prototype.contains = function(text) {
+  return this.indexOf(text) !== -1;
+};
+
+// Add Event Listeners
+document.addEventListener('DOMContentLoaded', restore_options);
+
+for (i = 0; i <  selectorList.length; i++) {
+  selectorList[i].addEventListener('click', optionCaller,false);
+  selectorList[i].addEventListener('mouseover', handleMouseover, false);
+  selectorList[i].addEventListener('mouseleave', handleMouseout, false);
+}
+
+useCustomSearch.addEventListener('click', function() {
+  updateCustomSearchView();
+});
+
+for (i = 0; i <  advanced_settings.length; i++) {
+  advanced_settings[i].addEventListener('click', advancedSettingsCaller, false);
+}
+
+document.getElementById('custom_engine_update').addEventListener('click', function() {
   var element = document.getElementById('custom_engine');
   if(useCustomSearch.checked){
     if(element.value.toLowerCase().contains("http")){
@@ -141,7 +182,6 @@ document.getElementById('custom_settings_update').addEventListener('click', func
 document.getElementById('additional-settings-toggle').addEventListener('click', function() {
   var settingsPane = document.getElementById('expandable_settings_pane');
   var engines = document.getElementById('custom-engine-select');
-  console.log(engines)
 
   if (settingsPane.className.search('open') >= 0) {
     removeClass(settingsPane, 'open');
@@ -152,17 +192,3 @@ document.getElementById('additional-settings-toggle').addEventListener('click', 
     addClass(engines, 'hidden');
   }
 });
-
-function addClass(element, classNameToAdd) {
-  if (!element.className.includes(classNameToAdd)) {
-    element.className = element.className + ' ' + classNameToAdd;
-  }
-}
-
-function removeClass(element, classNameToAdd) {
-  element.className = element.className.replace(classNameToAdd, '');
-}
-
-String.prototype.contains = function(text) {
-  return this.indexOf(text) !== -1;
-};
